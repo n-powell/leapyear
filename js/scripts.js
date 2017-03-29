706c823cdce55f045e247647663f2fdb1c619654
Nick Powell and Trevor Fast <nick@>
2017-03-29 09:31:56 -0700

    add last condition to the if statement.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var leapYear = function(year) {
-      if ((year % 100 !== 0) && (year % 4 === 0)) {
+      if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
         return true;
       } else {
         return false;
